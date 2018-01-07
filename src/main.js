62a65b16101de8d4fd8da13ee24a194a56b646ef
王祥宇 <god@wangxiangyudeMacBook-Pro.local>
2018-01-07 16:02:13 +0800

    配置element-ui,axios

--- src/main.js
+++ src/main.js
@@ -3,13 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import axios from 'axios'
+Vue.prototype.$ajax = axios
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  render: h => h(App),
   template: '<App/>',
-  components: { App }
+  components: { App },
+  method: {
+    submitForm () {
+    this.$ajax({
+     method: 'post',
+     url: '/user',
+     data: {
+     name: 'wise',
+     info: 'wrong'
+     }
+    })
+   }
+  }
 })
